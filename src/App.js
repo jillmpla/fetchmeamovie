16cdea16c01055f5aa22a8468ad4bcd31580815b
Jill M Platts <contactjillplatts@gmail.com>
2025-08-27 16:39:31 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MovieList from "./components/MovieList";
+import MovieDetail from "./components/MovieDetail";
+import "./App.css";
+
+function App(){
+    return (
+        <Router>
+            <div className="app-shell">
+                <Header />
+
+                <main id="main" role="main">
+                    <Routes>
+                        <Route path="/" element={<MovieList />} />
+                        <Route path="/movies/:id" element={<MovieDetail />} />
+                    </Routes>
+                </main>
+
+                <Footer />
+            </div>
+        </Router>
+    );
+}
+export default App;
+
